04600d1a7d7febff58afc27afccecaae2cb4d124
tymur <tmaksymovych@kse.org.ua>
2026-03-28 14:46:57 +0200

    init, forgot to make interim commits

---
+++ Week8/DeliveryService.cs
@@ -0,0 +1,51 @@
+﻿namespace Week8;
+    
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; }
+    public double Weight { get; }
+
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+       Console.WriteLine($"Track № {TrackingNumber}, Weight: {Weight}");
+    }
+}
+
+public class Letter : DeliveryItem
+{
+    public Letter(string trackNum, double weight) : base(trackNum, weight) {}
+    public override double CalculateCost()
+    {
+        return (Weight * 10) + 15;
+    }
+}
+
+public class Parcel : DeliveryItem
+{
+    public string Dimensions { get; }
+
+    public Parcel(string trackNum, double weight, string dimensions) : base(trackNum, weight)
+    {
+        Dimensions = dimensions;
+    }
+
+    public override double CalculateCost()
+    {
+        return (Weight * 25) + 50;
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($"Dimensions: {Dimensions}");
+    }
+}
