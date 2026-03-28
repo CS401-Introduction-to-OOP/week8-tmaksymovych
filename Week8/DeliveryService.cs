d2ab42fd2ee3891014ff94bf9c750607ce7ec022
tymur <tmaksymovych@kse.org.ua>
2026-03-28 15:03:02 +0200

    added Generics CargoContainer<T>.

--- Week8/DeliveryService.cs
+++ Week8/DeliveryService.cs
@@ -49,3 +49,24 @@ public class Parcel : DeliveryItem
         Console.WriteLine($"Dimensions: {Dimensions}");
     }
 }
+
+public class CargoContainer<T> where T : DeliveryItem
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (var item in _items)
+        {
+            totalCost += item.CalculateCost();
+        }
+
+        return totalCost;
+    }
+}
