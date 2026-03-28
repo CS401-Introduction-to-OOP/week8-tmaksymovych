855e31d00e057304a910b619ad6737d81f9a6bc3
tymur <tmaksymovych@kse.org.ua>
2026-03-28 15:38:32 +0200

    end!

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -2,5 +2,24 @@ namespace Week8;
 
 public class Program
 {
+    static void Main()
+    {
+    Letter l1 = new Letter("2354", 1.6);
+    Letter l2 = new Letter("2355", 0.3);
+    Parcel p1 = new Parcel("123", 6.7, "30x20x15");
+    Parcel p2 = new Parcel("124", 23, "30x20x15");
     
+    l1.PrintInfo();
+    p2.PrintInfo();
+
+    CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+    myCargo.AddItem(l1);
+    myCargo.AddItem(l2);
+    myCargo.AddItem(p1);
+    myCargo.AddItem(p2);
+
+    double TC = myCargo.GetTotalCost();
+    Console.WriteLine($"Total Coat of all deliveries: {TC}");
+    
+    }
 }
